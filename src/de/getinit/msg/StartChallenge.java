17565d51e506fe55f855f4654f13bb18d8addd0c
DeBukkIt <debukkit@googlemail.com>
2020-07-16 10:06:06 +0200

    lexicographic permutation brute force implemented

--- src/de/getinit/msg/StartChallenge.java
+++ src/de/getinit/msg/StartChallenge.java
@@ -1,8 +1,8 @@
 package de.getinit.msg;
 
 import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class StartChallenge {
@@ -24,11 +24,10 @@ public class StartChallenge {
 		List<Node> shortestPath = findShortestPath(nodes, 0);
 
 		// Print result
-		System.out.println("\nShortest of all paths found. Order of waypoints is:");
-		for (Node n : shortestPath) {
-			System.out.println(n);
-		}
+		System.out.println("\nShortest of all paths found. Order of waypoints is: " + listToString(shortestPath));	
 	}
+	
+	private static int[] nodeOrder;
 
 	/**
 	 * Finds the shortest path possible between the nodes of the given list where
@@ -36,36 +35,93 @@ public class StartChallenge {
 	 * other nodes are in between.
 	 * 
 	 * @param unsortedNodes The list of nodes to order so the distance of a path between them all is the shortest possible
-	 * @param startEndNodeIndex The index of the start and end node
+	 * @param baseNodeIndex The index of the start and end node
 	 * @return the ordered list of nodes so the distance of a path between them all is the shortest possible
 	 */
-	private static List<Node> findShortestPath(List<Node> unsortedNodes, int startEndNodeIndex) {
+	private static List<Node> findShortestPath(List<Node> unsortedNodes, int baseNodeIndex) {
 		// remove start/end node from source list
-		List<Node> unsortedNodesWithoutStartEnd = new ArrayList<Node>(unsortedNodes);
-		unsortedNodesWithoutStartEnd.remove(startEndNodeIndex);
+		List<Node> unsortedNodesWithoutBase = new ArrayList<Node>(unsortedNodes);
+		unsortedNodesWithoutBase.remove(baseNodeIndex);
 
-		// try random solutions, find shortest of them
-		List<Node> shortestPath = null;
-		double shortestCircleLength = Double.MAX_VALUE;
-
-		for (long l = 0; l < Long.MAX_VALUE; l++) {
-			List<Node> candidatePath = new ArrayList<Node>(unsortedNodesWithoutStartEnd);
-			// randomize
-			Collections.shuffle(candidatePath);
-			candidatePath.add(0, unsortedNodes.get(startEndNodeIndex));
-			// measure
-			if (pathLengthCircular(candidatePath) < shortestCircleLength) {
-				// if new shortest found:
-				shortestPath = candidatePath;
-				shortestCircleLength = pathLengthCircular(candidatePath);
-				System.out.println("New shortest path is " + (int) (shortestCircleLength / 1000) + " km short: " + listToString(shortestPath));
-			}
+		// find shortest path in between
+		// set initial order of nodes
+		nodeOrder = new int[unsortedNodesWithoutBase.size()];
+		for(int i = 0; i < nodeOrder.length; i++) {
+			nodeOrder[i] = i;
 		}
-
-		// just add the end node before returning
-		shortestPath.add(unsortedNodes.get(startEndNodeIndex));
+		// iterate over all possible orders
+		List<Node> shortestPath = new ArrayList<Node>();
+		double shortestPathLength = Integer.MAX_VALUE;
+		int orderCounter = 0;
+		do {
+			// delcare path candidate
+			List<Node> candidatePath = new ArrayList<Node>(unsortedNodesWithoutBase.size() + 1);
+			// interate order rule set, build new candiadate path
+			for(int i = 0; i < nodeOrder.length; i++) {
+				candidatePath.add(i, unsortedNodesWithoutBase.get(nodeOrder[i]));
+			}
+			candidatePath.add(0, unsortedNodes.get(baseNodeIndex));
+			// check if a new shorter path was found
+			double candidatePathLength = pathLengthCircular(candidatePath);
+			if(candidatePathLength < shortestPathLength) {
+				shortestPath = candidatePath;
+				shortestPathLength = candidatePathLength;
+				System.out.println("New shortest path found. Length=" + (int) (shortestPathLength/1000) + "km. Path= [via " + listToString(shortestPath) + " back to Base]");
+			}
+			
+			// keep track of progress
+			if((orderCounter++ + 1) % (1 * 1000 * 1000) == 0) {
+				double progress = (double) orderCounter / (double) fac(nodeOrder.length) * (double) 100;
+				System.out.println(new DecimalFormat("0.0000000000000000").format(progress) + "% done.");				
+			}
+			
+		} while(generateNextOrder());
+		
+		// just add the base node as endNode before returning
+		shortestPath.add(unsortedNodes.get(baseNodeIndex));
 
 		return shortestPath;
+	}
+	
+	static int debug = 0;
+	
+	private static boolean generateNextOrder() {		
+		int i = nodeOrder.length - 1;
+	    while (i > 0 && nodeOrder[i - 1] >= nodeOrder[i])
+	        i--;
+
+	    if (i <= 0)
+	        return false;
+	    
+	    int j = nodeOrder.length - 1;
+	    while (nodeOrder[j] <= nodeOrder[i - 1])
+	        j--;
+		
+		swap(nodeOrder, i - 1, j);
+
+		j = nodeOrder.length - 1;
+	    while (i < j) {
+	        int temp = nodeOrder[i];
+	        nodeOrder[i] = nodeOrder[j];
+	        nodeOrder[j] = temp;
+	        i++;
+	        j--;
+	    }
+		
+		return true;
+	}
+	
+	private static void swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	private static long fac(long i) {
+		if(i == 1) {
+			return 1;
+		}
+		return i * fac(i - 1);
 	}
 
 	/**
